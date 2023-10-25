efddc02d1129c2b6572cb4d49aeee4d72495eb75
Valiant Dmello <vvd0927@gmail.com>
2023-10-24 23:47:09 -0400

    settings

--- elearningsite/settings.py
+++ elearningsite/settings.py
@@ -10,10 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-from pathlib import Path
-from django.core.mail import send_mail
 import django_heroku
 import dj_database_url
+from pathlib import Path
+from django.core.mail import send_mail
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -36,7 +37,7 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
